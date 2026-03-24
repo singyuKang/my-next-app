a4f8f5f51f02b4c8f9a88a49bb6eb11c3cb9ab40
singyuKang <rkdtlscks123@naver.com>
2026-03-24 11:08:04 +0900

    feat: next.js 버전 업데이트

--- next.config.ts
+++ next.config.ts
@@ -25,12 +25,10 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
+  turbopack: {},
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
